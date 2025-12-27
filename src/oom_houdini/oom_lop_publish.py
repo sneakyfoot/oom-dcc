f40ca0db68ae68e922f11b1125e94dfd98f4e0ff
ez <evan_zazula@me.com>
2025-12-26 16:29:21 -0800

    updates to version loading for new wedged usd publishes and HDA updates

--- src/oom_houdini/oom_lop_publish.py
+++ src/oom_houdini/oom_lop_publish.py
@@ -17,7 +17,7 @@ version_menu = _cache.version_menu
 def populate_lop(kwargs: dict) -> None:
     """Callback for USD LOP publish name parameter.
 
-    Builds the publish file path using the ``oom_usd_publish`` ShotGrid
+    Builds the publish file path using the ``oom_usd_publish_wedged`` ShotGrid
     template and updates version tracking parameters.
     """
     tk = hou.session.oom_tk
@@ -55,10 +55,25 @@ def populate_lop(kwargs: dict) -> None:
     final = os.path.join(dir_expr, file_expr)
     node.parm("filename").set(final)
 
+    # set pre and post wedge file string for houdini wedge reading
+    WEDGE_EXPR = '`chs("wedge_index")`'
+
+    full_path = os.path.join(dir_expr, file_expr)  # same as “final”
+    idx = full_path.find(WEDGE_EXPR)
+
+    pre_wedge = full_path[:idx]  # dir_expr + base + first dot
+    post_wedge = full_path[idx + len(WEDGE_EXPR) :]  # ".$F4.bgeo.sc"
+
+    node.parm("filename_pre_wedge").set(pre_wedge)
+    node.parm("filename_post_wedge").set(post_wedge)
+
     frame_path = final.replace(".usd", ".$F4.usd")
     node.parm("filename_frames").set(frame_path)
 
-    versions = get_versions(publish_name, "oom_usd_publish")
+    post_wedge_frames = post_wedge.replace(".usd", ".$F4.usd")
+    node.parm("filename_post_wedge_frames").set(post_wedge_frames)
+
+    versions = get_versions(publish_name, "oom_usd_publish_wedged")
     versions = _cache.ensure_spare_versions_initialized(node, versions)
     store_versions(node, versions)
     cache_versions_update(publish_name, versions)
@@ -68,13 +83,13 @@ def populate_lop(kwargs: dict) -> None:
 def refresh_versions(kwargs: dict) -> None:
     """Refresh version list for USD publishes only.
 
-    Ensures the query is constrained to PublishedFileType 'oom_usd_publish'
+    Ensures the query is constrained to PublishedFileType 'oom_usd_publish_wedged'
     so similarly named cache publishes don’t collide.
     """
     node = kwargs["node"]
     publish_name = node.parm("name").eval()
 
-    versions = get_versions(publish_name, "oom_usd_publish")
+    versions = get_versions(publish_name, "oom_usd_publish_wedged")
     store_versions(node, versions)
     cache_versions_update(publish_name, versions)
     restore_selected(node)
